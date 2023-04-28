e88f617d8740a8be6c0385e953524f315c6f7841
sonwezali <al1.sonmez.mi@gmail.com>
2023-04-28 03:15:10 +0300

    Created JavaLsp.java
    Java LSP for nvim

---
+++ src/JavaLsp.java
@@ -0,0 +1,12 @@
+public class JavaLsp {
+
+    public static void main(String[] args) {
+       cryForLsp(); 
+    }
+
+    public static void cryForLsp() {
+        System.out.println("I need a working LSP for java :(");
+    }
+
+}
+
